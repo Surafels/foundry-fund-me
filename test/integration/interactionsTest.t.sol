0d69d49ca895a787dee6dc70d3a54d8bc85d5f8c
Surafels <ssurafel4@gmail.com>
2024-08-21 20:39:55 +0300

    Add tests

---
+++ test/integration/interactionsTest.t.sol
@@ -0,0 +1,49 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/interaction.s.sol";
+
+contract InteractionsTest is Test {
+    FundMe fundMe;
+    address USER = makeAddr("USER");
+    uint256 constant SEND_VALUE = 1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+    // function testUserCanFundInteractions() public {
+    //     FundFundMe fundFundMe = new FundFundMe();
+    //     vm.prank(USER);
+    //     vm.deal(USER, 1e18);
+    //     fundFundMe.fundFundMe(address(fundMe));
+
+    //     address funder = fundMe.getFunders(0);
+    //     assertEq(funder, USER);
+    // }
+
+    
+    function testUserCanFundAndOwnerWithdraw() public {
+        uint256 preUserBalance = address(USER).balance;
+        uint256 preOwnerBalance = address(fundMe.getOwner()).balance;
+
+        // Using vm.prank to simulate funding from the USER address
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+
+        uint256 afterUserBalance = address(USER).balance;
+        uint256 afterOwnerBalance = address(fundMe.getOwner()).balance;
+
+        assert(address(fundMe).balance == 0);
+        assertEq(afterUserBalance + SEND_VALUE, preUserBalance);
+        assertEq(preOwnerBalance + SEND_VALUE, afterOwnerBalance);
+    }
+}
