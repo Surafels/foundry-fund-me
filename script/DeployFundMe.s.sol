72002a708ba662d0787dc55013c0cba480e49a92
Surafels <ssurafel4@gmail.com>
2024-08-21 20:38:21 +0300

    Add scripts

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployFundMe is Script {
+
+
+// Before startBroadcast -> Not a real transaction
+    function run()  external returns (FundMe) {
+        // Before startBroadcast -> Not a real transaction
+
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
+        // after startBroadcast -> a real transaction
+
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
+        vm.stopBroadcast();
+        return fundMe; 
+
+    }
+
+
+}
